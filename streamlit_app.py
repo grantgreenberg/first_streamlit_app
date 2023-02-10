2e7f1e67b327eb4c718c4011e7dafefca89c8b64
Grant Greenberg <ggreenberg620@gmail.com>
2023-02-10 15:28:49 -0700

    Update streamlit_app.py
    Move filter above table.

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,9 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-streamlit.dataframe(my_fruit_list)
 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+streamlit.dataframe(my_fruit_list)
+
+
